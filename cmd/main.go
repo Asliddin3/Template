0c4912ccd0c5d3d6c7dd7cfcb1e4626994115bb9
Asliddin3 <asliddindeh@mail.ru>
2022-10-06 20:10:59 +0500

    fix bug

--- cmd/main.go
+++ cmd/main.go
@@ -22,8 +22,7 @@ func main() {
 		logger.String("host", cfg.PostgresHost),
 		logger.Int("port", cfg.PostgresPort),
 		logger.String("database", cfg.PostgresDatabase))
-
-	connDB, err := db.ConnectToDB(cfg)
+	connDB, err := db.ConnectToDb(cfg)
 	if err != nil {
 		log.Fatal("sqlx connection to postgres error", logger.Error(err))
 	}
