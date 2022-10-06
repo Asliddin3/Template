064640ff2049962928f7f6bedfa23443f72a7b40
Asliddin3 <asliddindeh@mail.ru>
2022-10-06 20:09:36 +0500

    added net

--- storage/postgres/user.go
+++ storage/postgres/user.go
@@ -1,20 +1,25 @@
 package postgres
 
 import (
-	"github.com/jmoiron/sqlx"
 	pb "github.com/Asliddin3/Template/genproto"
+	"github.com/jmoiron/sqlx"
 )
 
-type userRepo struct{
+type userRepo struct {
 	db *sqlx.DB
 }
 
-func (r *userRepo) Create(user *pb.User) (*pb.User,error){
-	userRepo :=pb.User{}
-	err:=r.db.QueryRow(`insert into users(name,last_name) values($1,$2)
-	returning id,name,last_name`,user.Name,user.LastName).Scan(&userRepo.Id,&userRepo.Name,&userRepo.LastName)
+// NewUserRepo ...
+func NewUserRepo(db *sqlx.DB) *userRepo {
+	return &userRepo{db: db}
+}
+
+func (r *userRepo) Create(user *pb.User) (*pb.User, error) {
+	userResp := pb.User{}
+	err := r.db.QueryRow(`insert into users (name, last_name) values($1, $2) returning id, name, last_name`, user.Name, user.LastName).Scan(&userResp.Id, &userResp.Name, &userResp.LastName)
 	if err != nil {
 		return &pb.User{}, err
 	}
-	return &userRepo,nil
+
+	return &userResp, nil
 }
---
+++ storage/storage.go
@@ -0,0 +1,29 @@
+package storage
+
+import (
+	"github.com/Asliddin3/Template/storage/postgres"
+	"github.com/Asliddin3/Template/storage/repo"
+	"github.com/jmoiron/sqlx"
+)
+
+//IStorage ...
+type IStorage interface {
+	User() repo.UserStorageI
+}
+
+type storagePg struct {
+	db       *sqlx.DB
+	userRepo repo.UserStorageI
+}
+
+//NewStoragePg ...
+func NewStoragePg(db *sqlx.DB) *storagePg {
+	return &storagePg{
+		db:       db,
+		userRepo: postgres.NewUserRepo(db),
+	}
+}
+
+func (s storagePg) User() repo.UserStorageI {
+	return s.userRepo
+}
